5d6918102b078b666a4f585bcee32439281966bf
JoeriRietjens <45871940+JoeriRietjens@users.noreply.github.com>
2020-10-05 13:48:16 +0200

    Added Spring Maven logic layer
    Added the logic of our mastermind project.

---
+++ MastermindLogic/src/main/java/nl/fhict/s3/mastermindlogic/MastermindlogicApplication.java
@@ -0,0 +1,13 @@
+package nl.fhict.s3.mastermindlogic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MastermindlogicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MastermindlogicApplication.class, args);
+	}
+
+}
