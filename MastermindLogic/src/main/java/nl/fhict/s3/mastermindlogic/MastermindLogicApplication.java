4d2b75afc4a1ed1fdcb93149a3c33f5103828f02
Siora van Weperen <428439@student.fontys.nl>
2020-10-06 09:39:42 +0200

    Update spelling of Logic

--- MastermindLogic/src/main/java/nl/fhict/s3/mastermindlogic/MastermindLogicApplication.java
+++ MastermindLogic/src/main/java/nl/fhict/s3/mastermindlogic/MastermindLogicApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MastermindlogicApplication {
+public class MastermindLogicApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MastermindlogicApplication.class, args);
+		SpringApplication.run(MastermindLogicApplication.class, args);
 	}
 
 }
