07c3d1e634d1c0b4346f63b70d3a334f45e4f5ed
SanderWieringa <43203835+SanderWieringa@users.noreply.github.com>
2020-10-12 20:42:22 +0200

    enum error fixed, code tests added

--- MastermindLogic/src/test/java/nl/fhict/s3/mastermindlogic/MastermindlogicApplicationTests.java
+++ MastermindLogic/src/test/java/nl/fhict/s3/mastermindlogic/MastermindlogicApplicationTests.java
@@ -3,9 +3,12 @@ package nl.fhict.s3.mastermindlogic;
 import nl.fhict.s3.mastermindlogic.entity.Board;
 import nl.fhict.s3.mastermindlogic.entity.Colour;
 import nl.fhict.s3.mastermindlogic.entity.EPinColour;
-import nl.fhict.s3.mastermindlogic.entity.Game;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import java.util.ArrayList;
 
 @SpringBootTest
 class MastermindlogicApplicationTests {
@@ -13,6 +16,28 @@ class MastermindlogicApplicationTests {
 	@Test
 	void contextLoads() {
 
+	}
+
+	@Test
+	void testThreeEntitiesColorCode_ShouldReturnNotEqual() {
+		Colour[] code = {new Colour(1, EPinColour.BlUE), new Colour(1, EPinColour.YELLOW), new Colour(1, EPinColour.GREEN)};
+		Board board = new Board(1, code);
+
+		int expected = 4;
+		int actual = code.length;
+
+		assertNotEquals(expected, actual);
+	}
+
+	@Test
+	void testFourEntitiesColorCode_ShouldReturnEqual() {
+		Colour[] code = {new Colour(1, EPinColour.BlUE), new Colour(1, EPinColour.YELLOW), new Colour(1, EPinColour.GREEN), new Colour(1, EPinColour.ORANGE)};
+		Board board = new Board(1, code);
+
+		int expected = 4;
+		int actual = code.length;
+
+		assertEquals(expected, actual);
 	}
 
 }
