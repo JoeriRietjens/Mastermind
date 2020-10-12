61ae053c4cbde7fc08357b4cd8d09526616b230f
Gino <ginovandegraaf@hotmail.com>
2020-10-12 15:05:24 +0200

    Setup Test For MasterMind Logic

--- MastermindLogic/src/test/java/nl/fhict/s3/mastermindlogic/MastermindlogicApplicationTests.java
+++ MastermindLogic/src/test/java/nl/fhict/s3/mastermindlogic/MastermindlogicApplicationTests.java
@@ -10,4 +10,29 @@ class MastermindlogicApplicationTests {
 	void contextLoads() {
 	}
 
+
+	@Test
+	void testWongColorDecision()
+	{
+		//2 player game one make a guess but it is wrong.
+	}
+
+	@Test
+	void winGame()
+	{
+		//the colors are all right and player wins game.
+
+	}
+
+	@Test
+	void setupColorCode()
+	{
+		//player setup a color pallet.
+	}
+	@Test
+	void colorTwoPinsRight()
+	{
+		//only two colors are right on the board. the result is that 2 pins are right.
+	}
+
 }
