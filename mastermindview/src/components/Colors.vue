5f56c56dd28117c662f49327a3aec46ffd84b83f
Siora van Weperen <428439@student.fontys.nl>
2020-10-12 16:36:47 +0200

    update Vue

--- mastermindview/src/components/Colors.vue
+++ mastermindview/src/components/Colors.vue
@@ -1,11 +1,15 @@
 <template>
     <div>
-        <span class="dot" id="green"></span> <br>
-        <span class="dot" id="yellow"></span> <br>
-        <span class="dot" id="blue"></span> <br>
-        <span class="dot" id="red"></span> <br>
-        <span class="dot" id="white"></span> <br>
-        <span class="dot" id="black"></span> <br>
+        <div class="row">
+            <span class="dot" id="green"></span> <br>
+            <span class="dot" id="yellow"></span> <br>
+            <span class="dot" id="blue"></span> <br>
+        </div>
+        <div class="row">
+            <span class="dot" id="red"></span> <br>
+            <span class="dot" id="white"></span> <br>
+            <span class="dot" id="black"></span> <br>
+        </div>
     </div>
 </template>
 
@@ -20,8 +24,8 @@ export default {
 
 <style scoped>
 .dot {  
-    height: 40px;
-    width: 40px;
+    height: 50px;
+    width: 50px;
     border-radius: 50%;
     display: inline-block;
     border: groove;
@@ -48,5 +52,10 @@ export default {
 
 #black {  
     background-color: black;
+}
+
+.row {
+    display: inline-block;
+    position: relative;
 }
 </style>
