0726404df59cd12e48bfd429eb9327bfd91b1017
Siora van Weperen <428439@student.fontys.nl>
2020-12-07 15:41:11 +0100

    adding application class and making game id an UUID

---
+++ MastermindLogic/src/main/java/nl/fhict/s3/mastermindlogic/entity/Application.java
@@ -0,0 +1,38 @@
+package nl.fhict.s3.mastermindlogic.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class Application {
+
+    private static Application INSTANCE;
+    private List<Game> games = new ArrayList<>();
+
+    public static Application getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new Application();
+        }
+        return INSTANCE;
+    }
+
+    public Game getOpenGame() {
+        for (Game game :
+                games) {
+            if (game.getPlayers()[1] == null) {
+                return game;
+            }
+        }
+        Game game = new Game();
+        games.add(game);
+        return game;
+    }
+
+    public Game getGameById(UUID id) {
+        for(Game game: games){
+            if(game.getId() == id )
+                return game;
+        }
+        return null;
+    }
+}
--- MastermindLogic/src/main/java/nl/fhict/s3/mastermindlogic/entity/Game.java
+++ MastermindLogic/src/main/java/nl/fhict/s3/mastermindlogic/entity/Game.java
@@ -2,30 +2,25 @@ package nl.fhict.s3.mastermindlogic.entity;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 @Scope("singleton")
 
 public class Game {
 
-    private final int id;
+    private final UUID id;
     private Player[] players = new Player[2];
 
     public Player[] getPlayers() {
         return players;
     }
-    public int getId() {
+    public UUID getId() {
         return id;
     }
 
     public Game() {
-        this.id = 0;
-        Player player1 = new Player(0);
-        Player player2 = new Player(1);
-        players[0] = player1;
-        players[1] = player2;
-    }
-    public Game(int id) {
-        this.id = id;
+        this.id = UUID.randomUUID();
         Player player1 = new Player(0);
         Player player2 = new Player(1);
         players[0] = player1;
