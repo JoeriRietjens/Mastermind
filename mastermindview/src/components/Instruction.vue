b5917c992532fb81a4f3f87ceb436e95c6ca0253
Gino <ginovandegraaf@hotmail.com>
2020-12-07 15:29:21 +0100

    update instruction text pre added

--- mastermindview/src/components/Instruction.vue
+++ mastermindview/src/components/Instruction.vue
@@ -2,17 +2,18 @@
 
 
    <div>
+     <pre> 
 De speler plaatst 4 gekleurde codepinnetjes in de gaatjes 
 aan de voorkant van zijn speelbord.
-<br>De code bestaat uit ieder gewenste combinatie van de 6 kleuren. 
+De code bestaat uit ieder gewenste combinatie van de 6 kleuren. 
 Je mag 2 of meer codepinnetjes van dezelfde kleur gebruiken.
-<br>Je tegenstander doet hetzelfde en stelt ook een code op.
+Je tegenstander doet hetzelfde en stelt ook een code op.
 
 Wanneer de code is opgezet, kan de tegenspeler beginnen met raden.
-<br>Dit doe je door een rij van 4 gekleurde codepinnetjes op het spelbord
+Dit doe je door een rij van 4 gekleurde codepinnetjes op het spelbord
 van de tegenstander te zetten. Naarmate het spel vordert, krijg je meer
 informatie over de code en kun je steeds gerichtere pogingen wagen. 
-<br>Elke rij
+Elke rij
 codepinnetjes blijft gedurende de ronde staan. Na iedere poging moet de
 codemaker de tegenstander vertellen wat er goed en fout is in het rijtje.
 Dat gebeurt door middel van zwarte en witte scorepinnetjes.
@@ -30,7 +31,7 @@ witte pinnetje in de scoregaten niet van belang.
 
 Laat een scoregaatje leeg voor elk codepinnetje van een verkeerde kleur.
 De codekraker heeft maximaal 10 kansen om de code te raden
-
+</pre>
 </div>
 
 </template>
