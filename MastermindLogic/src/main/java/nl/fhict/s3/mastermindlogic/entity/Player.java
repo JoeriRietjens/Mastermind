6a47aa90e55f04169fee7e5c54c61d84083fe3fb
JoeriRietjens <45871940+JoeriRietjens@users.noreply.github.com>
2021-01-04 13:26:33 +0100

    Clean up
    Deleted unused methods and specified imports

--- MastermindLogic/src/main/java/nl/fhict/s3/mastermindlogic/entity/Player.java
+++ MastermindLogic/src/main/java/nl/fhict/s3/mastermindlogic/entity/Player.java
@@ -25,20 +25,6 @@ public class Player {
         return id;
     }
 
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public Board getBoard() {
         return board;
     }
